75fc4b09a5c968c4b29e5e3cf22c37c266df0e04
Pablo Alan <pabloxt14@gmail.com>
2025-10-10 18:15:52 -0300

    refactor: set tailwind to v3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
     <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+      className="bg-zinc-950 min-h-screen"
     >
-      <h2>Hello World</h2>
+      <h2 className="text-zinc-50">Hello World</h2>
     </div>
   );
 }
