a7fd4c308464932c5628f076916e3048835be92c
Pablo Alan <pabloxt14@gmail.com>
2025-10-12 20:00:25 -0300

    docs: learn image and link components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-950">
-      <h2 className="text-zinc-50">Hello World</h2>
+      <h2 className="text-zinc-50">Home</h2>
+
+      <Link
+        href="/users"
+        className="mt-4 cursor-pointer text-zinc-50 hover:underline"
+      >
+        Users page
+      </Link>
     </div>
   )
 }
