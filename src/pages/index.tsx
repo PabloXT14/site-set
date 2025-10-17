8d7e9f4ebc813942acbea35cc6c986b1a6cef63c
Pablo Alan <pabloxt14@gmail.com>
2025-10-17 19:18:25 -0300

    feat: create header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button"
+import { Header } from "@/components/header"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-zinc-950 p-4">
-      <h2 className="text-zinc-50">Home</h2>
-
-      <Button>Click me</Button>
+    <div className="min-h-screen bg-zinc-900">
+      <Header />
     </div>
   )
 }
