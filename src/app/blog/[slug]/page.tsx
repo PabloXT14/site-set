6e4e9b1a6b8c47233a93c990b0836d0597e75399
Pablo Alan <pabloxt14@gmail.com>
2025-12-09 17:37:19 -0300

    refactor: understand SSG and ISR on app-router

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,6 +9,17 @@ type Params = {
   }>
 }
 
+// SSG no App Router
+// generateStaticParams serve para gerar parâmetros dinâmicos para as páginas dinâmicas, como o slug
+export async function generateStaticParams() {
+  return await allPosts.map((post) => ({
+    slug: post.slug,
+  }))
+}
+
+// ISR no App Router
+export const revalidate = 60 // revalidate every 60 seconds
+
 export default async function BlogPostPage({ params }: Params) {
   const { slug } = await params
   const post = allPosts.find((p) => p.slug === slug)
